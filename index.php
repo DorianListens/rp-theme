b790aeefa1551b6054133e59e7e9500a3ae76ee5
Dorian <dorian.scheidt@gmail.com>
2014-03-04 19:59:29 -0500

    Fix main orbit slider

--- index.php
+++ index.php
@@ -6,15 +6,6 @@
 			
 				    <div id="main" class="large-12 medium-12 columns clearfix" role="main">
 
-				    	  <div class="row">
-
-						    <div class="large-12 columns"><div id="bigvid" class='flex-video vimeo widescreen'>
-						      <iframe src="//player.vimeo.com/video/87322941?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
-						    </div>
-						    <a id="main-vid-link" href="#" class="right">More About This Film</a>
-						  </div>
-						<div class="row"><br /></div>
-
 						<?php get_template_part( 'partials/loop', 'rp-slider' ); ?>
 						<?php get_template_part( 'partials/loop', 'rp-threeup'); ?>
 			
--- partials/loop-rp-slider.php
+++ partials/loop-rp-slider.php
@@ -1,51 +1,48 @@
-<div class="row"> <!-- Main Orbit Slider -->
-    <div class="large-12 columns">
-    	<ul class="example-orbit" data-orbit data-options="bullets: false; timer: false; slide_number: false;">
-	      	<li>
-	      		<div class="row">
-					  <?php
-					  // New Query
-						$args = array( 'post_type' => 'rp_films', 'posts_per_page' => 5 );
-					 	$loop = new WP_Query( $args );
-						while ( $loop->have_posts() ) : $loop->the_post(); 
-					  // Get the terms
-					  $category = get_the_terms( get_the_ID(), 'film_cat' );
-					  $film_type = get_the_terms( get_the_ID(), 'film_type' );
-					  ?>
-					  <div class="large-2 small-4 columns">
-					  	<a class="rp-vid-link" href="#" data-src="<?php get_video_src(); ?>" data-link="<?php the_permalink(); ?>">
-					  		<?php the_post_thumbnail(); ?>
-					  	
-					  	<h4 class="slider-film-title"><?php the_title(); ?></h4>
-	              		<h5 class="slider-film-type"><?php echo $film_type->name; ?></h5>
-	              		</a>
-					  </div>
 
-	 
-	  				<?php endwhile;?>
-				</div> <!--End Orbit Row -->
-			</li>	<!-- End Orbit Item -->
-			<li>
-				<div class="row">
-					<?php
-				  	// New Query
-					$args2 = array( 'post_type' => 'rp_films', 'posts_per_page' => 5, 'offset' => 5 );
-				 	$loop2 = new WP_Query( $args2 );
-					while ( $loop2->have_posts() ) : $loop2->the_post(); 
-				  	// Get the terms
-				  	$category = get_the_terms( get_the_ID(), 'film_cat' );
-				  	$film_type = get_the_terms( get_the_ID(), 'film_type' );
-				  	?>
-				  	<div class="large-2 small-4 columns">
-					  	<a class="rp-vid-link" href="#" data-src="<?php get_video_src(); ?>" data-link="<?php the_permalink(); ?>">
-					  		<?php the_post_thumbnail(); ?>
-					  	</a>
-					  	<h4 class="slider-film-title"><?php the_title(); ?></h4>
-	              		<h5 class="slider-film-type"><?php echo $film_type->name; ?></h5>
-					</div>
-					<?php endwhile;?>
-				</div> <!--End Orbit Row -->
-			</li>	<!-- End Orbit Item -->
-		</ul> 
+<div class="row">
+<?php // New Query
+$args = array( 'post_type' => 'rp_films', 'posts_per_page' => 20 );
+$loop = new WP_Query( $args );
+if ($loop->have_posts()) : $counter = 0; ?>
+ 	
+<?php while ( $loop->have_posts() ) : ?>
+	<?php if ($counter == 0){$loop->the_post();} ?> <!-- First Film -->
+	    <div class="large-12 columns"><div id="bigvid" class='flex-video vimeo widescreen'>
+	      <iframe src="<?php get_video_src(); ?>" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
+	    </div>
+	    <a id="main-vid-link" href="<?php the_permalink(); ?>" class="right">More About This Film</a>
 	</div>
-</div> <!-- End Orbit -->
+	<div class="row"><br /></div>
+		<div class="row"> <!-- Main Orbit Slider -->
+		    <div class="large-12 columns">
+		    	<ul class="example-orbit" data-orbit data-options="bullets: false; timer: false; slide_number: false;">
+			      	<li> <!-- First Orbit Slide -->
+			      		<div class="row">
+						<?php $counter++; ?>
+						<?php while ($counter < 7) : 
+
+					  	// Get the terms
+						 $category = get_the_terms( get_the_ID(), 'film_cat' );
+						 $film_type = get_the_terms( get_the_ID(), 'film_type' );
+						 ?>
+						  <div class="large-2 small-4 columns">
+						  	<a class="rp-vid-link" href="#" data-src="<?php get_video_src(); ?>" data-link="<?php the_permalink(); ?>">
+						  		<?php the_post_thumbnail(); ?>
+							  	<h4 class="slider-film-title"><?php the_title(); ?></h4>
+			              		<h5 class="slider-film-type"><?php echo $film_type->name; ?></h5>
+		              		</a>
+						  </div>
+		  				<?php $counter++; 
+		  				$loop->the_post();	 ?>
+		  				<?php endwhile; ?>
+						</div> <!--End Orbit Row -->
+					</li>	<!-- End Orbit Slide -->
+					<li> <!-- New Orbit Slide -->
+						<div class="row">
+					<?php $counter = 1; ?>
+					<?php $loop->the_post(); ?>
+				<?php endwhile; ?>
+			<?php endif; ?>
+				</ul> 
+			</div>
+		</div> <!-- End Orbit -->
